40149874039faf838740fcbf936ad12e16acc0f6
170332rajeshkumar@gmail.com <170332rajeshkumar@gmail.com>
2022-05-10 05:45:17 +0000

    date updated

--- models/customers.sql
+++ models/customers.sql
@@ -1,1 +1,4 @@
-select * from raw.jaffle_shop.customers where hire_date = '{{ var("hire_date") }}'
+
+
+--select * from raw.jaffle_shop.customers where hire_date =  
+select '{{ var("hire_date") }}'
